8dec46a3f41d27e111a1960ce952f1ab638058d3
amar21jeet <amarjeetrana1947@gmail.com>
2024-08-28 14:48:04 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert() --> we are using nodejs not browser ,it can't run like that different syntax
+
+// weakly typed language incompetable data can be assigned
+let a = "amar"
+a = 4
+// console.log(a)
+/* primitive
+1) number ==> range in 2^53
+2) bigint ==> for large numbers
+3) string ==> "sequence of charaters" (prefer -" ")
+4) boolean ==> true/false
+5) undifined ==> value not assigned to variable(type == object)
+6) null ==> stand alone value(type == object)
+7) symbols ==> uniqueness(used in react component)
+
+non-primitives
+1) object
+*/ 
+// Data Types in Js
+
+console.log(typeof null)
