8bae62c167e8eab9108919af67000afaaec7e78e
amar21jeet <amarjeetrana1947@gmail.com>
2024-09-09 21:20:20 +0000

    objects part-2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,18 @@
+// nested object
+const regularUser = {
+    email: "some@google.com",
+    fullName: {
+        userName: {
+            firstName: "Amarjeet",
+            lastName: "Rana"
+        }
+    }
+}
+// console.log(regularUser.fullName.userName.firstName);
+
+// merge two objects
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+// const obj3 = {...obj1, ...obj2}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
