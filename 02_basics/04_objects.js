e1f8d3477ed08b7c637a7c1a538847750b0e43e2
amar21jeet <amarjeetrana1947@gmail.com>
2024-09-15 08:14:53 +0000

    changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -15,6 +15,36 @@ const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
 // const obj3 = {...obj1, ...obj2}
 const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+// console.log(obj3);
 
+// ********************  object de-structuring  *****************************
 
+const course = {
+    name : "JS in hinds",
+    price : "999",
+    courseInstructor : "Hitesh"
+}
+// extracting keys from object
+// syntax:
+//      const {key} = object_name   
+//        OR
+//      cont {key:newkeyName to be used as} = object_name
+ const {courseInstructor} = course
+// console.log(courseInstructor) // Hitesh
+
+const {courseInstructor : instructor} = course
+// console.log(instructor); // Hitesh
+
+// ********* API --> JSON API as object*******************
+// {
+//     "name": "Amarjeet", 
+//     "coursename": "Js in hindi", 
+//     "price": "free"
+// }
+
+// API as array
+  [
+    {},
+    {},
+    {}
+  ]
