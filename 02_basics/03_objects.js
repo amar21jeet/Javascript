02f70648f3c048f66c97e200719397e4cbb41a86
amar21jeet <amarjeetrana1947@gmail.com>
2024-09-15 07:31:21 +0000

    changes done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -12,23 +12,40 @@ const JsUser = {
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
 
 // change value or overwrite value
 JsUser.email = "amar@chatgpt.com";
-console.log(JsUser.name);
+// console.log(JsUser.name);
 
 // freezing object to prevent properties overwrite
 // Object.freeze(JsUser);
 
 JsUser.email = "amar@microsoft.com"
-console.log(JsUser);
+// console.log(JsUser);
 
 // function as property in object
 JsUser.greeting = function(){
     console.log(`Hello, JS user! ${this.name}`)
 }
 // console.log(JsUser.greeting) // it function reference
-console.log(JsUser.greeting())
+// console.log(JsUser.greeting())
+
+// keys of object as array
+const keyArr = Object.keys(JsUser)
+// console.log(keyArr)
+
+// values of object as array
+const valueArr = Object.values(JsUser)
+// console.log(valueArr);
+
+// key value pair of object
+const keyValueArr = Object.entries(JsUser)
+// console.log(keyValueArr);
+
+// check object has specified property or not return boolean value
+// console.log(JsUser.hasOwnProperty("email"));
+
+
