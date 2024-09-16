ca9be9a1e15136dbc6e683ff7ebd865d0ac8e965
amar21jeet <amarjeetrana1947@gmail.com>
2024-09-16 16:48:30 +0000

    Arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,13 @@
+const addTwo = (num1, num2) => {
+    return num1+ num2
+}
+// console.log(addTwo(3, 6));
+
+// implicit arrow function  // used in react
+  const sumOfNum = (num1, num2) => (num1+num2)
+ // sumOfNum(2, 3)
+ 
+// return object with in ()
+ const chai = () => ({username:"amarjeet"})
+ console.log(chai());
+ 
