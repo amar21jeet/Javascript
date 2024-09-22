74a290c904abf474464197af7ae4bb3cc13a5904
amar21jeet <amarjeetrana1947@gmail.com>
2024-09-22 10:41:15 +0000

    while and do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+// while
+// entry control loop check condition before executing statement
+
+let x = 1
+while (x < 10) {
+   // console.log(x);
+    x++
+}
+
+// do while
+// exit control loop first statement execute then condition check
+// it logged value to console once even if the condition fails
+
+let score = 1
+do {
+    console.log(score);
+    score++
+}
+while(score < 1)
